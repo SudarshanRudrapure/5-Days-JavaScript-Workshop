91300d01941c361708569a77b1fbbb1885cd56ef
SudarshanRudrapure <sudarshanrudrapure@gmail.com>
2026-02-17 10:05:48 +0530

    JavaScript Basics

--- Day-1/assignment 02/script.js
+++ Day-1/assignment 02/script.js
@@ -2,7 +2,6 @@ let principalamount=10000;
 let interest=5;
 let time=2;
 
-
 document.writeln("principalamount: "+principalamount+"<br>");
 document.writeln("interest: "+interest+"<br>");
 document.writeln("time: "+time+"<br>");
